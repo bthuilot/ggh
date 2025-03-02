eece3e20978ebf0160797b78cdbced05cc271af8
Bryce Thuilot <bryce@thuilot.io>
2025-03-02 17:04:44 -0500

    fix(cli): align CLI args and help message
    Signed-off-by: Bryce Thuilot <bryce@thuilot.io>

--- bin/commands.ml
+++ bin/commands.ml
@@ -9,24 +9,28 @@
 module Git = Ggh.Git
 module Log = Dolog.Log
 
+(** [hooks_dir] is the directory that the global hooks will live.Arith_status
+    each item in the directory should be a system link to the binary ggh. *)
+let hooks_dir = "/usr/local/ggh"
+
+(** [hooks] is the list of supported hooks for ggh *)
+let hooks = [ "pre-commit"; "commit-msg"; "pre-push" ]
+
 (** [help] prints the CLI help menu *)
 let help () =
   print_endline "Usage: ggh [COMMAND] [OPTIONS...]\n";
   print_endline "Commands:";
   print_endline
-    "  install            Installs this executable as the global git config";
+    ("  install            Installs this executable to hooks path '" ^ hooks_dir
+   ^ "' (should run as sudo)");
+  print_endline
+    "  configure          Sets the 'core.hooksPath' in the global git \
+     configuration (should run after 'install' as user)";
   print_endline "  print-opts         Prints the environment variable options";
   print_endline "";
   print_endline "Options:";
   (* print_endline "  --version          Print version information and exit."; *)
   print_endline "  --help             Print this help message and exit."
-
-(** [hooks_dir] is the directory that the global hooks will live.Arith_status
-    each item in the directory should be a system link to the binary ggh. *)
-let hooks_dir = "/usr/local/bin/_ggh"
-
-(** [hooks] is the list of supported hooks for ggh *)
-let hooks = [ "pre-commit"; "commit-msg"; "pre-push" ]
 
 (** [install] installs the current running binary as the core hooks path in the
     users gitconfig by first system linking it self as each hook in [hooks]
@@ -44,5 +48,18 @@ let install (_ : string array) =
       Unix.symlink bin_path sym_link)
     hooks
 
+(** [configure] configures the global git config setting the 'core.hooksPath'
+    value to the directory [hooks_dir]. This should be ran after [install] to
+    ensure the dirrectory is correctly set up. *)
 let configure (_ : string array) =
   Git.set_config_value ~scope:Git.Global ~group:"core" "hooksPath" hooks_dir
+
+(** [print_env_opts] prints the environment variables that can be set to
+    configure the application. *)
+let print_env_opts (_ : string array) =
+  print_endline "Environment variable options:\n";
+  print_endline
+    "  GGH_HOOK_OVERRIDE        Overrides hook name read from Sys.argv.(0)";
+  print_endline
+    "  GGH_USER_STDERR          Uses STDERR for logging rather than log file";
+  print_endline "  GGH_LOG_LEVEL            Override log level read from config"
