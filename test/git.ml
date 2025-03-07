9e8032ad5e58bb940cf7589ba633178d2a970f21
Bryce Thuilot <bryce@thuilot.io>
2025-03-07 16:57:20 -0500

    test: add test scaffolding
    Signed-off-by: Bryce Thuilot <bryce@thuilot.io>

---
+++ test/git.ml
@@ -0,0 +1,45 @@
+module Git = Ggh.Git
+
+let gitScopePP fmt scope = Format.fprintf fmt "%s" (Git.scope_to_string scope)
+
+let test_repo_config_get () =
+  let mock_keygroup = "gghtest"
+  and mock_keysub = "test-repo-config-get"
+  and mock_val = "testing123456790" in
+  Git_mock.write_repo_config (mock_keygroup ^ "." ^ mock_keysub) mock_val;
+  let result = Git.get_config_value ~group:mock_keygroup mock_keysub in
+  (* before running test, clear entry *)
+  Git_mock.clear_values [ mock_keygroup ^ "." ^ mock_keysub ];
+
+  Alcotest.(check (option (pair (testable gitScopePP ( = )) string)))
+    "failed to get git config value"
+    (Some (Git.Local, mock_val))
+    result
+
+let test_repo_config_get_multiple () =
+  let mock_keygroup = "gghtest"
+  and mock_keysub = "test-repo-config-get-multiple"
+  and mock_val_repo = "testing123456790"
+  and mock_val_global = "1234567890testing" in
+  let mock_key = mock_keygroup ^ "." ^ mock_keysub in
+  Git_mock.write_repo_config mock_key mock_val_repo;
+  Git_mock.write_global_config mock_key mock_val_global;
+  let result = Git.get_config_values ~group:mock_keygroup mock_keysub in
+  (* before running test, clear entry *)
+  Git_mock.clear_values [ mock_key ];
+
+  Alcotest.(check (option (list (pair (testable gitScopePP ( = )) string))))
+    "failed to get git config value"
+    (Some [ (Git.Local, mock_val_repo); (Git.Global, mock_val_global) ])
+    result
+
+let () =
+  Alcotest.run "git.ml"
+    [
+      ( "config",
+        [
+          Alcotest.test_case "get config value" `Slow test_repo_config_get;
+          Alcotest.test_case "get config values" `Slow
+            test_repo_config_get_multiple;
+        ] );
+    ]
---
+++ test/git_mock.ml
@@ -0,0 +1,36 @@
+(* *)
+module Log = Dolog.Log
+
+exception GitFailure of string
+
+let write_repo_config (key : string) (value : string) : unit =
+  let cmd = "git config set " ^ key ^ " " ^ value in
+  match Sys.command cmd with
+  | 0 -> ()
+  | _ -> raise (GitFailure "unable to write repo config")
+
+let write_global_config (key : string) (value : string) : unit =
+  let cmd = "git config set --global " ^ key ^ " " ^ value in
+  match Sys.command cmd with
+  | 0 -> ()
+  | _ -> raise (GitFailure "unable to write global config")
+
+let print_repo_config ?(all : bool = false) (key : string) : unit =
+  let cmd = "git config get " ^ (if all then " --all " else "") ^ key in
+  match Sys.command cmd with
+  | 0 -> ()
+  | _ -> raise (GitFailure "unable to read repo config")
+
+let print_global_config ?(all : bool = false) (key : string) : unit =
+  let cmd =
+    "git config get --global " ^ (if all then " --all " else "") ^ key
+  in
+  match Sys.command cmd with
+  | 0 -> ()
+  | _ -> raise (GitFailure "unable to read repo config")
+
+let clear_values (keys : string list) : unit =
+  List.map (fun k -> (k, Sys.command ("git config --unset-all " ^ k))) keys
+  |> List.iter (function
+       | _, 0 -> ()
+       | k, _ -> raise (GitFailure ("failed to remove key " ^ k)))
